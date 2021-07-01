464752d70b0a1b86d59f554e4831c2a4dff2fa40
alin <rglin2020@gmail.com>
2021-07-01 22:55:08 +0800

    添加第三方服务整合模块

---
+++ boutique-third-party/src/main/java/com/boutique/thirdparty/BoutiqueThirdPartyApplication.java
@@ -0,0 +1,13 @@
+package com.boutique.thirdparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoutiqueThirdPartyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoutiqueThirdPartyApplication.class, args);
+    }
+
+}
---
+++ boutique-third-party/src/test/java/com/boutique/thirdparty/BoutiqueThirdPartyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boutique.thirdparty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoutiqueThirdPartyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
