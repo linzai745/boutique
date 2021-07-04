3b0b44055f981620282229fc5092393bc9deffe2
alin <rglin2020@gmail.com>
2021-07-04 08:56:29 +0800

    aliyun oss 服务端签名后直传

---
+++ boutique-third-party/src/main/java/com/boutique/thirdparty/controller/OSSController.java
@@ -0,0 +1,69 @@
+package com.boutique.thirdparty.controller;
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.common.utils.BinaryUtil;
+import com.aliyun.oss.model.MatchMode;
+import com.aliyun.oss.model.PolicyConditions;
+import com.boutique.common.utils.R;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/thirdparty")
+public class OSSController {
+
+    @Value("${alibaba.cloud.access-key}")
+    private String accessId;
+
+    @Value("${alibaba.cloud.oss.endpoint}")
+    private String endpoint;
+
+    @Autowired
+    private OSS ossClient;
+
+    @PostMapping("/aliyun/oss/signature")
+    public R signature() {
+        String bucket = "boutique-media"; // 请填写您的 bucketname 。
+        String host = "https://" + bucket + "." + endpoint; // host的格式为 bucketname.endpoint
+        String dir = new SimpleDateFormat("yyyy-MM-dd").format(new Date()) + "/"; // 用户上传文件时指定的前缀。
+        Map<String, String> respMap = new LinkedHashMap<>();
+
+        try {
+            long expireTime = 30;
+            long expireEndTime = System.currentTimeMillis() + expireTime * 1000;
+            Date expiration = new Date(expireEndTime);
+            // PostObject请求最大可支持的文件大小为5 GB，即CONTENT_LENGTH_RANGE为5*1024*1024*1024。
+            PolicyConditions policyConds = new PolicyConditions();
+            policyConds.addConditionItem(PolicyConditions.COND_CONTENT_LENGTH_RANGE, 0, 1048576000);
+            policyConds.addConditionItem(MatchMode.StartWith, PolicyConditions.COND_KEY, dir);
+
+            String postPolicy = ossClient.generatePostPolicy(expiration, policyConds);
+            byte[] binaryData = postPolicy.getBytes("utf-8");
+            String encodedPolicy = BinaryUtil.toBase64String(binaryData);
+            String postSignature = ossClient.calculatePostSignature(postPolicy);
+
+            respMap.put("accessid", accessId);
+            respMap.put("policy", encodedPolicy);
+            respMap.put("signature", postSignature);
+            respMap.put("dir", dir);
+            respMap.put("host", host);
+            respMap.put("expire", String.valueOf(expireEndTime / 1000));
+            // respMap.put("expire", formatISO8601Date(expiration));
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            ossClient.shutdown();
+        }
+
+        return R.ok().put("data", respMap);
+    }
+}
